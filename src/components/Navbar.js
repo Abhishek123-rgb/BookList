a18de60bccebbcb1dbcec0ced66e9e7b2e6dc87e
Abhishek Kumar <abhishek020898@gmail.com>
2021-03-15 11:14:31 +0530

    somechanges

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,9 @@ const Navbar = () => {
     const {book} = useContext(BooksContext);
     return(
       <div className="navbar">
+
         <h1> Ninja Reading List</h1>
+        <h2>Let's Fucked up all reactja project</h2>
         <p>Currently you have {book.length} books to get through...</p>
       </div>
     );
